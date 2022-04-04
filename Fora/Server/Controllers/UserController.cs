71a861bca190a8323710335e40defb725a0cf6f7
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-04 10:51:22 +0200

    Add UserController with simple requests

---
+++ Fora/Server/Controllers/UserController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Fora.Server.Data;
+namespace Fora.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        // GET: UserController
+        [HttpGet]
+        public IEnumerable<UserModel> Get()
+        {
+            return AppDbContext.Users;
+        }
+
+        // GET: UserController/Details/5
+        [HttpGet("{id}")]
+        public UserModel Details(int id)
+        {
+            return AppDbContext.Users.Where(u => u.Id == id).FirstOrDefault();
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create(UserModel user)
+        {
+            AppDbContext.Users.Add(user);
+            return Ok(user);
+        }
+
+        //// POST: UserController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: UserController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: UserController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: UserController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: UserController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
