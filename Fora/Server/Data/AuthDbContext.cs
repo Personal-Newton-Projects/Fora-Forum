778e296665a1c84b5e3cf5400b45a9fa13a2d3a6
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-04 12:27:02 +0200

    New AuthDbContext

---
+++ Fora/Server/Data/AuthDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fora.Server.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public DbSet<IdentityUser> IdentityUsers { get; set; }
+
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
