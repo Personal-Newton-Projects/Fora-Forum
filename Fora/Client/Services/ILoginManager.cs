e94c2e91172eb0c63fcd1b2562496c92927b615e
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-05 13:41:19 +0200

    Include new LoginManager methods in interface

--- Fora/Client/Services/ILoginManager.cs
+++ Fora/Client/Services/ILoginManager.cs
@@ -3,5 +3,8 @@
     public interface ILoginManager
     {
         Task LogInWithUser(string username, string password);
+        Task StoreUser(string id);
+        Task<bool> IsLoggedIn();
+        Task<UserModel> GetLoggedInUser();
     }
 }
