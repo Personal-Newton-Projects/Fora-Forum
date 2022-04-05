d2afd3629355ff2a482b3663fa23fd56a72fa0d5
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-05 19:20:46 +0200

    Remove SignUpPassword and create Migrations for it

---
+++ Fora/Server/Migrations/20220405165925_ThirdUpdateApp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fora.Server.Migrations.AuthDb
+{
+    public partial class ThirdUpdateApp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
