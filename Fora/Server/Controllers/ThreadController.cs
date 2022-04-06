6b7afaaf1f4979e513760ecb97dfe1ca019975b9
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-06 22:29:40 +0200

    Create ThreadController

---
+++ Fora/Server/Controllers/ThreadController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fora.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ThreadController : Controller
+    {
+        private readonly AppDbContext appDbContext;
+
+        public ThreadController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<ThreadModel> Get()
+        {
+            return appDbContext.Threads;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(ThreadModel thread)
+        {
+            await appDbContext.Threads.AddAsync(thread);
+            await appDbContext.SaveChangesAsync();
+            return Ok(thread);
+        }
+      
+    }
+}
