0d8835fc125e4a7f2bceac5f35f3667a98d9af03
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-06 19:13:15 +0200

    Rewrite GET and comment out POST in InterestController

--- Fora/Server/Controllers/InterestController.cs
+++ Fora/Server/Controllers/InterestController.cs
@@ -16,32 +16,28 @@ public class InterestController : Controller
     List<UserInterestModel> UserInterests { get; set; }
     
     [HttpGet]
-    public async Task<IActionResult> GetInterests()
+    public IEnumerable<InterestModel> GetInterests()
     {
-        return base.Ok(await GetDbInterests());
+        return appDbContext.Interests;
     }
-    [HttpGet]
-    public async Task<List<InterestModel>> GetDbInterests()
-    {
-        return await appDbContext.Interests.Include(i => i.UserInterests).ToListAsync();
-    }
+
     [HttpGet("{id}")]
     public InterestModel GetInterest(int id)
     {
         return appDbContext.Interests.Where(i => i.Id == id).FirstOrDefault();
     }
     
-    [HttpPost]
-    public async Task<IActionResult> AddInterestForUser(UserInterestModel userInterest)
-    {
-        // interest = appDbContext.Interests.FirstOrDefault(i => i.Id == id);
-        // await appDbContext.Interests.AddAsync(interest);
-        // await appDbContext.SaveChangesAsync();
-        // return interest
-        UserInterests.Add(userInterest);
-        userInterest.User.UserInterests.Add(userInterest);
-        return Ok(await GetDbInterests());
-    }
+    //[HttpPost]
+    //public async Task<IActionResult> AddInterestForUser(UserInterestModel userInterest)
+    //{
+    //    // interest = appDbContext.Interests.FirstOrDefault(i => i.Id == id);
+    //    // await appDbContext.Interests.AddAsync(interest);
+    //    // await appDbContext.SaveChangesAsync();
+    //    // return interest
+    //    UserInterests.Add(userInterest);
+    //    userInterest.User.UserInterests.Add(userInterest);
+    //    return Ok(await GetDbInterests());
+    //}
     
     
 }
