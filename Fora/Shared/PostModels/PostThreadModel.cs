e0a4d4ceb7fb8ed493209247d681a862bd13726f
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-08 15:53:33 +0200

    Create PostThreadModel and PostUserInterestsModel

---
+++ Fora/Shared/PostModels/PostThreadModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fora.Shared.PostModels
+{
+    public class PostThreadModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int InterestId { get; set; }
+        public int CreatorId { get; set; }
+    }
+}
---
+++ Fora/Shared/PostModels/PostUserInterestsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fora.Shared.PostModels
+{
+    public class PostUserInterestsModel
+    {
+        public int InterestsID { get; set; }
+        public int UserID { get; set; }
+    }
+}
