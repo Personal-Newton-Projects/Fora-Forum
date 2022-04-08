1e97bf15aaf300913f0caca3eccd62d0c1e1d3f4
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-08 11:30:14 +0200

    Create JsonDebug class

---
+++ Fora/Client/Managers/JsonDebug.cs
@@ -0,0 +1,10 @@
+﻿namespace Fora.Client.Managers
+{
+    public class JsonDebug
+    {
+        public JsonDebug(HttpResponseMessage message)
+        {
+            Console.WriteLine(message.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
