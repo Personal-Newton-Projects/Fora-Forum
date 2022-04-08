19468206c6ace22f0a8e56f2ac048e99c1a159aa
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-08 20:08:23 +0200

    PostUserUpdateModel

---
+++ Fora/Shared/PostModels/PostUserUpdateModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fora.Shared.PostModels
+{
+    public class PostUserUpdateModel
+    {
+        public int Id { get; set; }
+        public string? NewName { get; set; }
+        public bool BanUser { get; set; }
+        public bool DeleteUser { get; set; }
+
+    }
+}
