5713935ed1025e71ecadd33c819642a83f29cbc9
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-08 10:37:56 +0200

    Create FullyIncludedInterest that returns Interest with all fields

--- Fora/Server/Controllers/InterestController.cs
+++ Fora/Server/Controllers/InterestController.cs
@@ -16,15 +16,40 @@ public class InterestController : Controller
     List<UserInterestModel> UserInterests { get; set; }
     
     [HttpGet]
-    public IEnumerable<InterestModel> GetInterests()
+    public async Task<IEnumerable<InterestModel>> GetInterests()
     {
-        return appDbContext.Interests;
+        return await GetFullyIncludedInterests();
+    }
+
+    async Task<IEnumerable<InterestModel>> GetFullyIncludedInterests()
+    {
+        List<InterestModel> dbInterests = await appDbContext.Interests
+            .Include(i => i.UserInterests)
+            .Include(i => i.User)
+            .Include(i => i.Threads)
+            .ToListAsync();
+
+        foreach (InterestModel interest in dbInterests)
+        {
+
+            foreach (ThreadModel thread in appDbContext.Threads)
+            {
+                if(thread.Interest == interest)
+                {
+                    interest.Threads.Add(thread);
+                }
+            }
+
+        }
+
+        return dbInterests;
     }
 
     [HttpGet("{id}")]
-    public InterestModel GetInterest(int id)
+    public async Task<InterestModel> GetInterest(int id)
     {
-        return appDbContext.Interests.Where(i => i.Id == id).FirstOrDefault();
+        var result = await GetFullyIncludedInterests();
+        return result.SingleOrDefault(i => i.Id == id);
     }
 
     [HttpGet("/name/{name}")]
