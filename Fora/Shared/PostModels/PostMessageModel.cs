a904e389c8368b77325e1084a9901751549d4144
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-09 16:01:55 +0200

    Create PostMessageModel

---
+++ Fora/Shared/PostModels/PostMessageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fora.Shared.PostModels
+{
+    public class PostMessageModel
+    {
+        public int PosterId { get; set; }
+        public int ThreadId { get; set; }
+        public string Message { get; set; }
+
+    }
+}
