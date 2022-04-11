c3b23afde79c5a66e0dce146c598fbb04a68e73f
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-11 04:45:21 +0200

    Create new RoleModel and UserRoleModel

---
+++ Fora/Shared/RoleModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fora.Shared
+{
+    public enum RoleEnum
+    {
+        USER,
+        ADMIN
+    }
+    public class RoleModel
+    {
+        public int Id { get; set; }
+        public RoleEnum Role { get; set; }
+
+        public List<UserRoleModel> UserRoles { get; set; }
+
+    }
+}
---
+++ Fora/Shared/UserRoleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fora.Shared
+{
+    public class UserRoleModel
+    {
+        public int UserId { get; set; }
+        public UserModel User { get; set; }
+        public int RoleId { get; set; } = 1;
+        public RoleModel Role { get; set; }
+    }
+}
