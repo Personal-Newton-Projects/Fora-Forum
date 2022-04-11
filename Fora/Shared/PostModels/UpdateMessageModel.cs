abcc5fcd47a2ca0591579e8be9dbac8e2e92365c
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-11 02:00:58 +0200

    new UpdateMessage and UpdateThread models

---
+++ Fora/Shared/PostModels/UpdateMessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fora.Shared.PostModels
+{
+    public class UpdateMessageModel
+    {
+        public int ThreadId { get; set; }
+        public int MessageId { get; set; }
+        public string NewMessage { get; set; }
+    }
+}
---
+++ Fora/Shared/PostModels/UpdateThreadModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fora.Shared.PostModels
+{
+    public class UpdateThreadModel
+    {
+        public int ThreadId { get; set; }
+        public string NewName { get; set; }
+        public string? NewBody { get; set; }
+    }
+}
