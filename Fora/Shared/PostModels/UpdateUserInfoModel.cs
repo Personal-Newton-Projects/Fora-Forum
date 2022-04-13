0895886420b1b9b1514e66fa1df8c73fd3e3900b
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-14 01:15:06 +0200

    Add VerifyNewPassword and OldPassword field to UpdateUserInfoModel

--- Fora/Shared/PostModels/UpdateUserInfoModel.cs
+++ Fora/Shared/PostModels/UpdateUserInfoModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,10 @@ namespace Fora.Shared.PostModels
     public class UpdateUserInfoModel
     {
         public int Id { get; set; }
+        [Required]
         public string NewPassword { get; set; }
+        public string VerifyNewPassword { get; set; }
+        [Required]
+        public string OldPassword { get; set; }
     }
 }
