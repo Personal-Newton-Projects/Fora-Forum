dc5cff6985022e59aeeb1fb621f782fe04302906
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-04-14 09:25:26 +0200

    CreateInterestsModel: 3-25 characters allowed for Name field

--- Fora/Shared/PostModels/CreateInterestsModel.cs
+++ Fora/Shared/PostModels/CreateInterestsModel.cs
@@ -5,7 +5,7 @@ namespace Fora.Shared.PostModels;
 public class CreateInterestsModel
 {
     [Required]
-    [StringLength(15, MinimumLength = 3, ErrorMessage = "Interest name has to be 3-15 characters")]
+    [StringLength(25, MinimumLength = 3, ErrorMessage = "Interest name has to be 3-25 characters")]
     public string Name { get; set; }
     public int? UserID { get; set; }
 }
